a4fb3f965ba974f0d7b1338bb5f944a822c13a0a
RayyanZaid <rayyanzaid0401@gmail.com>
2024-06-04 11:33:20 -0700

    plz

---
+++ melody-metrics-hub/src/MainRoutes.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import StartPracticing from "./components/StartPracticing";
+import Confirmation from "./components/Confirmation";
+import TestCards from "./components/TestCards";
+import CardDetail from "./components/CardDetail";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+function MainRoutes() {
+  const [selectedCard, setSelectedCard] = useState(null);
+  const navigate = useNavigate();
+
+  const onCardClick = (card) => {
+    setSelectedCard(card);
+    navigate("/cardDetail");
+  };
+
+  return (
+    <Routes>
+      <Route path="/" element={<StartPracticing />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+      <Route
+        path="/testCards"
+        element={<TestCards onCardClick={onCardClick} />}
+      />
+      <Route path="/cardDetail" element={<CardDetail card={selectedCard} />} />
+    </Routes>
+  );
+}
+
+export default MainRoutes;
