41828290d4a270a3ee6c78ba4a28226625bb7721
RayyanZaid <rayyanzaid0401@gmail.com>
2024-06-04 19:31:47 -0700

    changed to nano ip

--- VisualNano/main.py
+++ VisualNano/main.py
@@ -51,7 +51,7 @@ def send_file_and_string(socket, client_id, filepath, sheetMusicName):
 # Set up ZMQ context and socket
 context = zmq.Context()
 socket = context.socket(zmq.ROUTER)  # ROUTER socket for more complex communication
-socket.bind("tcp://192.168.4.45:5555")  # Bind to port 5555
+socket.bind("tcp://192.168.7.191:5555")  # Bind to port 5555
 
 print("Visual Nano server started, waiting for client...")
 
